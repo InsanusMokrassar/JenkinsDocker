e1be082270230a7022f0db6cc6da263ba7028f27
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2023-11-18 16:43:07 +0600

    add autoupdater

---
+++ autoupdate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  newVersion="2.246.1"
+else
+  newVersion="$1"
+fi
+
+sed -i -E "s/[0-9\.]+\/jenkins.war/${newVersion}\/jenkins.war/g" Dockerfile
+sed -i -E "s/version=\"[0-9\.]+\"/version=\"${newVersion}\"/g" deploy
+
